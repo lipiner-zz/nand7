be9a76f15e5d4fa5680529633b18e8168935d4be
lipiner <ylipiner@gmail.com>
2017-11-12 22:55:00 +0200

    Hi

--- translator.py
+++ translator.py
@@ -12,12 +12,15 @@ REDUCE_MEMORY = "M=M-1" + END_OF_LINE_MARK
 INCREMENT_MEMORY = "M=M+1" + END_OF_LINE_MARK
 UPDATE_MEMORY_TO_D = "M=D" + END_OF_LINE_MARK
 ADDING_D_TO_MEMORY = "M=D+M" + END_OF_LINE_MARK
-SUBTRACTION_D_FROM_MEMORY = "M=M-D" + END_OF_LINE_MARK
+SUBTRACTION_D_FROM_M_TO_M = "M=M-D" + END_OF_LINE_MARK
+SUBTRACTION_M_FROM_D_TO_D = "D=D-M" + END_OF_LINE_MARK
 NEGATION_MEMORY = "M=-M" + END_OF_LINE_MARK
 NOT_MEMORY = "M=!M" + END_OF_LINE_MARK
 OR_D_MEMORY = "M=M|D" + END_OF_LINE_MARK
 AND_D_MEMORY = "M=M&D" + END_OF_LINE_MARK
 ADD_A_TO_D = "D=D+A" + END_OF_LINE_MARK
+FALSE_INTO_MEMORY = "M=0"
+TRUE_INTO_MEMORY = "M=-1"
 LABELS_TRANSLATOR = {"local": "LCL", "argument": "ARG", "this": "THIS", "that": "THAT"}
 POINTER_ADDRESS_TRANSLATOR = {"0": "THIS", "1": "THAT"}
 STACK = "SP"
@@ -27,7 +30,21 @@ TEMP_SEGMENT = "temp"
 CONSTANT_SEGMENT = "constant"
 A_PREFIX = "@"
 TEMP_MEMORY = "5"
-ADDR_STORE_REGISTER = "RAM13"
+ADDR_STORE_REGISTER = "R13"
+MY_TEMP_REGISTER = "R14"
+JUMP_ON_D = "D;"
+REGULAR_MINUS_LABEL = "REGULAR_MINUS_L"
+TRUE_LABEL = "TRUE_L"
+FALSE_LABEL = "FALSE_L"
+NEXT_COMMAND_LABEL = "CONTINUE_L"
+JUMP_NOT_NEGATIVE = "JGE"
+JUMP_NOT_POSITIVE = "JLE"
+JUMP_EQUAL = "JEQ"
+JUMP_ALWAYS = "0;JMP"
+JUMP_POSITIVE = "JGT"
+JUMP_NEGATIVE = "JLT"
+LABEL_PREFIX = "("
+LABEL_SUFFIX = ")"
 
 
 class Translator:
@@ -46,6 +63,8 @@ class Translator:
     @staticmethod
     def __reduce_stack():
         """
+        @SP
+        M=M-1
         :return: the asm code of reducing the stack
         """
         return Translator.__get_A_instruction(STACK) + REDUCE_MEMORY
@@ -53,6 +72,8 @@ class Translator:
     @staticmethod
     def __increment_stack():
         """
+        @SP
+        M=M+1
         :return: the asm code of reducing the stack
         """
         return Translator.__get_A_instruction(STACK) + INCREMENT_MEMORY
@@ -60,6 +81,10 @@ class Translator:
     @staticmethod
     def __operate_on_top_stack_value(operation):
         """
+        @SP
+        A=M
+        operation
+        :param operation: the operation on the top stack value (M)
         :return: the asm code fot getting the stack register into A
         """
         return Translator.__get_A_instruction(STACK) + GO_TO_REGISTER_M + operation
@@ -74,11 +99,22 @@ class Translator:
     @staticmethod
     def __operate_on_two_top_stack_values(operation):
         """
+        Inserts the top stack value into D and then the second top value into M and makes an operation on these
+        2 values
+        :param operation: the operation on the top stack value (D) and second (M)
         :return: the asm code for inserting the most top value in the stack into the A-register and the second
         top value into the memory M
         """
         return Translator.__operate_on_top_stack_value(GETTING_REGISTER_VALUE) + Translator.__reduce_stack() +\
                GO_TO_REGISTER_M + operation
+
+    @staticmethod
+    def __jump_based_on_D(condition):
+        """
+        :param condition: the asm syntax for jump condition
+        :return: the asm code for jumping based on the given condition and the value of D register
+        """
+        return JUMP_ON_D + condition
 
     @staticmethod
     def __translate_add():
@@ -93,7 +129,7 @@ class Translator:
         """
         :return: The asm code for the subtraction operation
         """
-        trans = Translator.__operate_on_two_top_stack_values(SUBTRACTION_D_FROM_MEMORY)
+        trans = Translator.__operate_on_two_top_stack_values(SUBTRACTION_D_FROM_M_TO_M)
         return trans
 
     @staticmethod
@@ -104,26 +140,71 @@ class Translator:
         trans = Translator.__operate_on_top_stack_value(NEGATION_MEMORY)
         return trans
 
-    @staticmethod
-    def __translate_eq():
+    def __compare(self, condition):
+        """
+        Computes the asm code for comparison between the 2 top values in the stack. The comparison condition is based
+        on the given parameter
+        :param: the comparison condition. The asm code of the jumping condition for true value. For example if the
+        result should be true for the second_top_value > first_top_value the condition should be JGT
+        :return: the comparison asm code
+        """
+        stack_value = Translator.__operate_on_top_stack_value(GETTING_REGISTER_VALUE)
+        temp_register_address = Translator.__get_A_instruction(MY_TEMP_REGISTER)
+        true_label_address = Translator.__get_A_instruction(TRUE_LABEL + str(self.__label_counter))
+        false_label_address = Translator.__get_A_instruction(FALSE_LABEL + str(self.__label_counter))
+        # gets the top stack value into a temp register
+        first_value_into_temp = stack_value + temp_register_address + UPDATE_MEMORY_TO_D
+        regular_minus_label_address = Translator.__get_A_instruction(REGULAR_MINUS_LABEL + str(self.__label_counter))
+        jump_if_not_negative = Translator.__jump_based_on_D(JUMP_NOT_NEGATIVE)
+        second_value = Translator.__reduce_stack() + GO_TO_REGISTER_M + GETTING_REGISTER_VALUE
+        jump_if_not_positive = Translator.__jump_based_on_D(JUMP_NOT_POSITIVE)
+
+        # set the result in case the top value is negative and the second is positive.
+        # If the compare condition is greater than zero, then the result should be true now. For other conditions
+        # the result should be false
+        if condition == JUMP_POSITIVE:
+            set_result = true_label_address + JUMP_ALWAYS
+        else:
+            set_result = false_label_address + JUMP_ALWAYS
+
+        # regular_minus_content: no overflow risk on subtraction, so subtracting the 2 values and jump based on the
+        # result to set the boolean value
+        regular_minus_label_title = self.__create_label(REGULAR_MINUS_LABEL)
+        regular_minus_content = stack_value + temp_register_address + SUBTRACTION_M_FROM_D_TO_D + true_label_address + \
+                                Translator.__jump_based_on_D(condition)
+        # the true and false labels - sets the stack value to the result of the comparison
+        false_label_title = self.__create_label(FALSE_LABEL)
+        false_label_content = Translator.__operate_on_top_stack_value(FALSE_INTO_MEMORY)
+        jump_next = Translator.__get_A_instruction(NEXT_COMMAND_LABEL + str(self.__label_counter)) + JUMP_ALWAYS
+        true_label_title = self.__create_label(TRUE_LABEL)
+        true_label_content = Translator.__operate_on_top_stack_value(TRUE_INTO_MEMORY)
+        next_command_label = self.__create_label(NEXT_COMMAND_LABEL)
+
+        # combines all the comparison code
+        trans = first_value_into_temp + regular_minus_label_address + jump_if_not_negative + second_value + \
+                regular_minus_label_address + jump_if_not_positive + set_result + regular_minus_label_title + \
+                regular_minus_content + false_label_title + false_label_content + jump_next + \
+                true_label_title + true_label_content + next_command_label
+        self.__label_counter += 1  # increment the label counter after this use
+        return trans
+
+    def __translate_eq(self):
         """
         :return: The asm code for the equal operation
         """
-        pass
+        return self.__compare(JUMP_EQUAL)
 
-    @staticmethod
-    def __translate_gt():
+    def __translate_gt(self):
         """
         :return: The asm code for the grater than operation
         """
-        pass
+        return self.__compare(JUMP_POSITIVE)
 
-    @staticmethod
-    def __translate_lt():
+    def __translate_lt(self):
         """
         :return: The asm code for the lower than operation
         """
-        pass
+        return self.__compare(JUMP_NEGATIVE)
 
     @staticmethod
     def __translate_and():
@@ -243,7 +324,6 @@ class Translator:
             return Translator.__reduce_stack() + Translator.__operate_on_top_stack_value(UPDATE_MEMORY_TO_D) + \
                    Translator.__get_A_instruction(POINTER_ADDRESS_TRANSLATOR[address]) + UPDATE_MEMORY_TO_D
 
-
     @staticmethod
     def __get_local_address(segment, address):
         """
@@ -323,3 +403,11 @@ class Translator:
         """
         return Translator.__get_A_instruction(address) + GETTING_ADDRESS_VALUE + \
                Translator.__operate_on_top_stack_value(UPDATE_MEMORY_TO_D)
+
+    def __create_label(self, label_name):
+        """
+        (label_name + INDEX)
+        :param label_name: the label name
+        :return: the label name format with the current label counter
+        """
+        return LABEL_PREFIX + label_name + str(self.__label_counter) + LABEL_SUFFIX
